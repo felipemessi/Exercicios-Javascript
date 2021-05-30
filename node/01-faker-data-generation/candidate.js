8d6e9c22a48dfac0dfbad33601b9c5f32b0a5e2d
felipemessi <felipemessibraga@gmail.com>
2021-05-29 21:10:42 -0300

    adding interisting codes

---
+++ node/01-faker-data-generation/candidate.js
@@ -0,0 +1,54 @@
+var fakerbr = require('faker-br');
+var faker = require('faker')
+var fs = require('fs')
+
+let data = []
+let count = 0
+while (count < 12) {
+  let cpf = fakerbr.br.cpf()
+  let name = faker.name.firstName()
+  name,
+  email,
+  cpf,
+  phone,
+  gender,
+  birthday,
+  country,
+  city,
+  neighborhood,
+  street,
+  homeNumber,
+  complement,
+  zipCode,
+  latitude,
+  longitude
+  // skills,
+
+
+  data.push({
+    name,
+    email,
+    cpf,
+    phone,
+    gender,
+    birthday,
+    country,
+    city,
+    neighborhood,
+    street,
+    homeNumber,
+    complement,
+    zipCode,
+    latitude,
+    longitude
+    // skills,
+  })
+  count ++
+}
+
+fs.writeFile('user.json', JSON.stringify(data), (err) => {
+  if (err) {
+      throw err;
+  }
+  console.log("JSON data is saved.");
+});
