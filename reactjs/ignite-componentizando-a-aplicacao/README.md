97901148f3f230ce1f2dcffccee2fab2734624a6
felipemessi <felipemessibraga@gmail.com>
2021-06-05 04:49:33 -0300

    make components content and navbar

---
+++ reactjs/ignite-componentizando-a-aplicacao/README.md
@@ -0,0 +1,8 @@
+# Watch Me
+
+## ignite-componentizando-a-aplicacao
+
+Desafil proposto no curso Ignite da Rocketseat. 
+
+Neste projeto foi trabalhado principalmente a componentização do layout.
+
---
+++ reactjs/ignite-componentizando-a-aplicacao/src/App.tsx
@@ -0,0 +1,15 @@
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+
+import './styles/global.scss';
+
+
+export function App() {
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar />
+      <Content />
+    </div>
+  )
+}
